1f9791fecbd435c044fc4dae9ecadab3c89834b8
vaahtokarkki <roni.lindholm@gmail.com>
2023-07-11 14:28:59 +0300

    chore(translation): update and compile finnish translations

--- src/quality_result_gui_plugin/resources/i18n/fi.ts
+++ src/quality_result_gui_plugin/resources/i18n/fi.ts
@@ -1,127 +1,127 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1">
+<TS version="2.1" language="fi_FI">
 <context>
     <name>@default</name>
     <message>
         <location filename="src/quality_result_gui/api/types/quality_error.py" line="36"/>
         <source>Attribute error</source>
-        <translation type="unfinished"></translation>
+        <translation>Attribuuttivirhe</translation>
     </message>
     <message>
         <location filename="src/quality_result_gui/api/types/quality_error.py" line="37"/>
         <source>Geometry error</source>
-        <translation type="unfinished"></translation>
+        <translation>Geometriavirhe</translation>
     </message>
     <message>
         <location filename="src/quality_result_gui/api/types/quality_error.py" line="38"/>
         <source>Topology error</source>
-        <translation type="unfinished"></translation>
+        <translation>Topologiavirhe</translation>
     </message>
     <message>
         <location filename="src/quality_result_gui/api/types/quality_error.py" line="39"/>
         <source>Continuity error</source>
-        <translation type="unfinished"></translation>
+        <translation>Jatkuvuusvirhe</translation>
     </message>
     <message>
         <location filename="src/quality_result_gui/api/types/quality_error.py" line="50"/>
         <source>Fatal</source>
-        <translation type="unfinished"></translation>
+        <translation>Hylkäävä</translation>
     </message>
     <message>
         <location filename="src/quality_result_gui/api/types/quality_error.py" line="51"/>
         <source>Warning</source>
-        <translation type="unfinished"></translation>
+        <translation>Vakava huomautus</translation>
     </message>
     <message>
         <location filename="src/quality_result_gui/api/types/quality_error.py" line="52"/>
         <source>Info</source>
-        <translation type="unfinished"></translation>
+        <translation>Info</translation>
     </message>
     <message>
         <location filename="src/quality_result_gui/quality_data_fetcher.py" line="49"/>
         <source>Checking for quality result updates</source>
-        <translation type="unfinished"></translation>
+        <translation>Päivitetään laatuajon tuloksia</translation>
     </message>
     <message>
         <location filename="src/quality_result_gui/quality_data_fetcher.py" line="50"/>
         <source>Quality check is in progress</source>
-        <translation type="unfinished"></translation>
+        <translation>Laatuajo on käynnissä</translation>
     </message>
     <message>
         <location filename="src/quality_result_gui/quality_data_fetcher.py" line="51"/>
         <source>Quality result update failed</source>
-        <translation type="unfinished"></translation>
+        <translation>Laatuajon tulosten päivittäminen epäonnistui</translation>
     </message>
     <message>
         <location filename="src/quality_result_gui/quality_data_fetcher.py" line="52"/>
         <source>Quality results are up to date</source>
-        <translation type="unfinished"></translation>
+        <translation>Laatuajon tulokset ovat päivitetty</translation>
     </message>
     <message>
         <location filename="src/quality_result_gui/quality_errors_filters.py" line="42"/>
         <source>Feature type</source>
-        <translation type="unfinished"></translation>
+        <translation>Kohdeluokan mukaan</translation>
     </message>
     <message>
         <location filename="src/quality_result_gui/quality_errors_filters.py" line="43"/>
         <source>Error type</source>
-        <translation type="unfinished"></translation>
+        <translation>Virhetyypin mukaan</translation>
     </message>
     <message>
         <location filename="src/quality_result_gui/quality_errors_filters.py" line="44"/>
         <source>Attribute Filter</source>
-        <translation type="unfinished"></translation>
+        <translation>Ominaisuustiedon mukaan</translation>
     </message>
     <message>
         <location filename="src/quality_result_gui/quality_errors_filters.py" line="114"/>
         <source>Select all</source>
-        <translation type="unfinished"></translation>
+        <translation>Valitse kaikki</translation>
     </message>
     <message>
         <location filename="src/quality_result_gui/quality_errors_filters.py" line="117"/>
         <source>Deselect all</source>
-        <translation type="unfinished"></translation>
+        <translation>Poista valinnat</translation>
     </message>
     <message>
         <location filename="src/quality_result_gui/quality_errors_tree_model.py" line="82"/>
         <source>Errors</source>
-        <translation type="unfinished"></translation>
+        <translation>Virheet</translation>
     </message>
     <message>
         <location filename="src/quality_result_gui/quality_errors_tree_model.py" line="83"/>
         <source>Error description</source>
-        <translation type="unfinished"></translation>
+        <translation>Virheen kuvaus</translation>
     </message>
     <message>
         <location filename="src/quality_result_gui/quality_error_manager.py" line="160"/>
         <source>Status of fetching quality result unknown</source>
-        <translation type="unfinished"></translation>
+        <translation>Laatuajon status ei tiedossa</translation>
     </message>
     <message>
         <location filename="src/quality_result_gui/quality_layer.py" line="95"/>
         <source>Quality errors</source>
-        <translation type="unfinished"></translation>
+        <translation>Laatuvirheet</translation>
     </message>
     <message>
         <location filename="src/quality_result_gui/ui/quality_errors_dock.py" line="76"/>
         <source>Toggle show errors on map filter</source>
-        <translation type="unfinished"></translation>
+        <translation>Näytä virheet kartalla</translation>
     </message>
     <message>
         <location filename="src/quality_result_gui/ui/quality_errors_tree_filter_menu.py" line="44"/>
         <source>Reset filters</source>
-        <translation type="unfinished"></translation>
+        <translation>Palauta oletussuodatukset</translation>
     </message>
     <message>
         <location filename="src/quality_result_gui_plugin/plugin.py" line="68"/>
         <source>Quality result GUI</source>
-        <translation type="unfinished"></translation>
+        <translation>Laatudialogi</translation>
     </message>
     <message>
         <location filename="src/quality_result_gui_plugin/plugin.py" line="76"/>
         <source>Development tools</source>
-        <translation type="unfinished"></translation>
+        <translation>Kehitystyökalut</translation>
     </message>
 </context>
 </TS>
