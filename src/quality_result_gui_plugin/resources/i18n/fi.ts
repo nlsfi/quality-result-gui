3d71ddfa6d3340621561ee10f26eb756755166e0
vaahtokarkki <roni.lindholm@gmail.com>
2023-07-11 09:20:12 +0300

    chore: upate translation file

--- src/quality_result_gui_plugin/resources/i18n/fi.ts
+++ src/quality_result_gui_plugin/resources/i18n/fi.ts
@@ -1,3 +1,127 @@
 <?xml version="1.0" encoding="utf-8"?>
-<!DOCTYPE TS><TS version="2.0">
+<!DOCTYPE TS>
+<TS version="2.1">
+<context>
+    <name>@default</name>
+    <message>
+        <location filename="src/quality_result_gui/api/types/quality_error.py" line="36"/>
+        <source>Attribute error</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/quality_result_gui/api/types/quality_error.py" line="37"/>
+        <source>Geometry error</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/quality_result_gui/api/types/quality_error.py" line="38"/>
+        <source>Topology error</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/quality_result_gui/api/types/quality_error.py" line="39"/>
+        <source>Continuity error</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/quality_result_gui/api/types/quality_error.py" line="50"/>
+        <source>Fatal</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/quality_result_gui/api/types/quality_error.py" line="51"/>
+        <source>Warning</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/quality_result_gui/api/types/quality_error.py" line="52"/>
+        <source>Info</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/quality_result_gui/quality_data_fetcher.py" line="49"/>
+        <source>Checking for quality result updates</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/quality_result_gui/quality_data_fetcher.py" line="50"/>
+        <source>Quality check is in progress</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/quality_result_gui/quality_data_fetcher.py" line="51"/>
+        <source>Quality result update failed</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/quality_result_gui/quality_data_fetcher.py" line="52"/>
+        <source>Quality results are up to date</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/quality_result_gui/quality_errors_filters.py" line="42"/>
+        <source>Feature type</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/quality_result_gui/quality_errors_filters.py" line="43"/>
+        <source>Error type</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/quality_result_gui/quality_errors_filters.py" line="44"/>
+        <source>Attribute Filter</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/quality_result_gui/quality_errors_filters.py" line="114"/>
+        <source>Select all</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/quality_result_gui/quality_errors_filters.py" line="117"/>
+        <source>Deselect all</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/quality_result_gui/quality_errors_tree_model.py" line="82"/>
+        <source>Errors</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/quality_result_gui/quality_errors_tree_model.py" line="83"/>
+        <source>Error description</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/quality_result_gui/quality_error_manager.py" line="160"/>
+        <source>Status of fetching quality result unknown</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/quality_result_gui/quality_layer.py" line="95"/>
+        <source>Quality errors</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/quality_result_gui/ui/quality_errors_dock.py" line="76"/>
+        <source>Toggle show errors on map filter</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/quality_result_gui/ui/quality_errors_tree_filter_menu.py" line="44"/>
+        <source>Reset filters</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/quality_result_gui_plugin/plugin.py" line="68"/>
+        <source>Quality result GUI</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/quality_result_gui_plugin/plugin.py" line="76"/>
+        <source>Development tools</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
 </TS>
